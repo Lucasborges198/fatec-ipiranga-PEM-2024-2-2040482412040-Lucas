edcc052998f11c8a91299afda5929c6304e9aa1a
Lucas Borges <lucasborgesribeiro198@gmail.com>
2024-09-07 23:46:22 -0300

    readded n1-2

--- PEM_2024-2-Atividades-N1-N2/PEM-Atividades-N1/PEM-Atividade-N1-2/n1-2atvdc.c
+++ PEM_2024-2-Atividades-N1-N2/PEM-Atividades-N1/PEM-Atividade-N1-2/n1-2atvdc.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-//Codigo feito pelo aluno Lucas Borges Ribeiro
+
 // Função para calcular a soma das notas centrais (excluindo a maior e a menor)
 float calcularNota(float notas[], int n) {
     float maior = notas[0], menor = notas[0], soma = 0;
@@ -14,8 +14,8 @@ float calcularNota(float notas[], int n) {
 }
 
 int main() {
-    int n, i, totalProvas;
-    float notaFinal = 0, mediaFinal = 0;
+    int n, i;
+    float notaFinal = 0;
 
     // Vetor para armazenar as notas
     float notas[100];
@@ -24,44 +24,45 @@ int main() {
     char* provas[] = {
         "Raciocinio logico", 
         "Conceitos de gerenciamento de projetos", 
-        "Língua inglesa", 
+        "Lingua inglesa", 
         "Conceitos de metodologia agil", 
         "Linguagem Javascript"
     };
 	
     for (i = 0; i < 5; i++) {
         // Entrada do número de notas para a prova atual
-        printf("Digite o numero de notas para a prova %s : ",provas[i]);
+        printf("Digite o numero de notas para a prova %s : ", provas[i]);
         scanf("%d", &n);
-		while (n<3){
-			if (n==0){
-				printf("Pulando para a proxima bateria!\n");
-				break;
-			}
-			printf("Numero minimo de notas nao foi atingido!\n");
-			printf("Digite o numero de notas para a prova %s : ",provas[i]);
-			scanf("%d", &n);
-		}
-			if(n>=3){	
-				printf("Digite as notas separadas por espaco: ");
-            	for (int j = 0; j < n; j++){
-            		scanf("%f", &notas[j]);
-        		}	
-			}
 
-        // Calcula a nota da prova atual
-        if(n>=3){
-        	float notaAtual = calcularNota(notas, n);
-        	notaFinal += notaAtual;
-        	// Exibe a nota da prova atual
-        	printf("Nota %s = %.0f\n", provas[i], resultadoNotas[0]);
-		
-		}
-}
+        // Validação para ter no mínimo 3 notas
+        while (n < 3) {
+            if (n == 0) {
+                printf("Pulando para a proxima bateria!\n");
+                break;
+            }
+            printf("Numero minimo de notas nao foi atingido!\n");
+            printf("Digite o numero de notas para a prova %s : ", provas[i]);
+            scanf("%d", &n);
+        }
 
-    // Exibe a nota final do candidato e a média final
-    printf("Notas finais das baterias: ");
-    printf("Nota final do candidato = %.0f\n", notaFinal);
+        // Processamento das notas, se houver pelo menos 3
+        if (n >= 3) {	
+            printf("Digite as notas separadas por espaco: ");
+            for (int j = 0; j < n; j++) {
+                scanf("%f", &notas[j]);
+            }
+
+            // Calcula a nota da prova atual
+            float notaAtual = calcularNota(notas, n);
+            notaFinal += notaAtual;
+
+            // Exibe a nota da prova atual
+            printf("Nota %s = %.2f\n", provas[i], notaAtual);
+        }
+    }
+
+    // Exibe a nota final do candidato
+    printf("Nota final do candidato = %.2f\n", notaFinal);
 
     return 0;
 }
